f5ad26181169c3d38cb6295457fe093ee5c11268
vabrea <99304763+vabrea@users.noreply.github.com>
2022-03-05 15:30:53 -0800

    Simple Calculator Completed
    Simple Calculator using HTML, CSS, and JS.

--- calculator.js
+++ calculator.js
@@ -3,12 +3,24 @@ function calculation(){
     //get values from the prompt and store them in the two vars.    
 
     let number1=Number(prompt("Enter in a number"));
+    let number2=Number(prompt("Enter another number"))
 
     // do the operation and store the results in vars (need four vars)
-    let sum=number1+10;
+    let sum=number1+number2;
+    let minus=number1-number2;
+    let multiply=number1*number2;
+    let divide=(number1/number2).toFixed(3);
+
+
     //display the result in the html
-    document.getElementById("results").innerHTML=`
-    <p>The sum is: ${sum} </p>
-    `;
+    document.getElementById("add").innerHTML=`
+    <p>${number1} + ${number2} = ${sum} </p>`
+    document.getElementById("sub").innerHTML=`
+    <p>${number1} - ${number2} = ${minus} </p>`
+    document.getElementById("multiply").innerHTML=`
+    <p>${number1} * ${number2} = ${multiply} </p>`
+    document.getElementById("divide").innerHTML=`
+    <p>${number1} / ${number2} = ${divide} </p>`
+
 }
     
